524d8edd02fa7dead6dcfc94b7e06c03ae20cdb8
Mavia-khan <mavia7517@gmail.com>
2021-06-24 21:47:54 +0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-var work = "on"
+Name                              Std_ID    CLASS_ID(107339)
+MAVIA ALAM KHAN                    64394
+DANIAL MIRZA                       64169
+MUNEEB RAFIQUE                     64401
