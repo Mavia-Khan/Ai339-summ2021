790cb7638661a8fe8f29014874e908c053465478
DaniyalMirza99 <82328789+DaniyalMirza99@users.noreply.github.com>
2021-07-18 12:22:12 +0500

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-Name                              Std_ID    CLASS_ID(107339)
-MAVIA ALAM KHAN                    64394
-DANIAL MIRZA                       64169
-MUNEEB RAFIQUE                     64401
